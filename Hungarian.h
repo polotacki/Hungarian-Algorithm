524031801ec09bb112fbb4dfb01e4b82ddcf643c
Polotacki <61323008+polotacki@users.noreply.github.com>
2023-01-14 06:00:18 +0200

    Update Hungarian.h

--- Hungarian.h
+++ Hungarian.h
@@ -1,14 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-// Hungarian.h: Header file for Class HungarianAlgorithm.
-// 
-// This is a C++ wrapper with slight modification of a hungarian algorithm implementation by Markus Buehren.
-// The original implementation is a few mex-functions for use in MATLAB, found here:
-// http://www.mathworks.com/matlabcentral/fileexchange/6543-functions-for-the-rectangular-assignment-problem
-// 
-// Both this code and the orignal code are published under the BSD license.
-// by Cong Ma, 2016
-// 
-
 #ifndef HUNGARIAN_H
 #define HUNGARIAN_H
 
